f282950fd95000e6787334772f86fe9404dec1d8
Maelye-ai <babalolachoumi@gmail.com>
2026-09-22 22:17:07 +0000

    Delete lea-carte-corrigee/lea-carte-corrigee directory

--- lea-carte-corrigee/lea-carte-corrigee/start_lea.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Lagunes Exploration Afrique - Visualisation 3D
-echo =====================================================================
-echo   Lancement de la Visualisation 3D - Lagunes Exploration Afrique
-echo =====================================================================
-echo   Demarrage du serveur local sur http://localhost:8080 ...
-start http://localhost:8080
-python server.py
-pause
